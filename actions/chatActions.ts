89132f688a31fe248ad82cb2697a46dddb34bde8
Saemi Park <saemiprk@gmail.com>
2024-09-01 10:16:22 +0900

    RLS

--- actions/chatActions.ts
+++ actions/chatActions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { createServerSupabaseAdminClient, createServerSupabaseClient } from "utils/supabase/server";
+import { createServerSupabaseAdminClient } from "utils/supabase/server";
 
 export const getAllUsers = async () => {
     const supabase = await createServerSupabaseAdminClient();
@@ -30,53 +30,3 @@ export async function getUserById(userId){
     return data.user;
 
 };
-
-export async function sendMessage({ message, chatUserId }) {
-  const supabase = await createServerSupabaseClient();
-
-  const {
-    data: { session },
-    error,
-  } = await supabase.auth.getSession();
-
-  if (error || !session.user) {
-    throw new Error("User is not authenticated");
-  }
-
-  const { data, error: sendMessageError } = await supabase
-    .from("message")
-    .insert({
-      message,
-      receiver: chatUserId,
-      sender: session.user.id,
-    });
-
-  if (sendMessageError) {
-    throw new Error(sendMessageError.message);
-  }
-
-  return data;
-}
-
-export async function getAllMessages({
-  chatUserId
-}){
-  const supabase = await createServerSupabaseClient();
-  const { data: { session }, error } = await supabase.auth.getSession();
-
-  if(error || !session.user){
-    throw new Error("User is not aythenticated");
-  }
-
-  const { data, error: getMessageError } = await supabase.from("message")
-  .select("*")
-  .or(`receiver.eq.${chatUserId}, reciver.eq.${session.user.id}`)
-  .or(`sender.eq.${chatUserId}, sender.eq.${session.user.id}`)
-  .order("created_at", {ascending: true});
-
-  if(getMessageError){
-    return [];
-  }
-
-  return data;
-}
